a886651e5c4990740ecaa241af4c1c1bc4f15bb0
vargas86 <vargas.juandedios@icloud.com>
2021-07-16 14:10:10 -0500

    add 404 & restore password

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,5 +1,14 @@
 export default function NotFound (){
     return (
-        <h1>NOT FOUND</h1>
+        <div>
+            <img className="d-block mx-auto w-100" src="https://11g-files-juandedios.s3.us-east-2.amazonaws.com/amparapp/ups404.png" alt="" />
+            <h1 className="text-center">Parece que estas perdido</h1>
+            <h2 className="text-center">Es momento de regresar</h2>
+
+            <img className="d-block mx-auto w-100" src="https://11g-files-juandedios.s3.us-east-2.amazonaws.com/amparapp/logo-slogan-icon.png" alt="" />
+            
+
+        </div>
+        
     )
 }
